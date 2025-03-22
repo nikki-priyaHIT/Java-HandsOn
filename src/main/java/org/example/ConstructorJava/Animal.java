4556babafe9c1c9949c0809c58f260b2ccfd6753
Nikki Priya <nikki.priya.it23@heritageit.edu.in>
2025-03-22 12:13:21 +0530

    Java HandsOn

---
+++ src/main/java/org/example/ConstructorJava/Animal.java
@@ -0,0 +1,37 @@
+package org.example.ConstructorJava;
+
+public class Animal {
+    int legs;
+    String type;
+    String name;
+    String color;
+
+    Animal(int legs, String type, String name, String color){
+        this.legs=legs;
+        this.type=type;
+        this.name=name;
+        this.color=color;
+    }
+
+    public Animal() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "legs=" + legs +
+                ", type='" + type + '\'' +
+                ", name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Animal obj = new Animal(4, "Dog", "Tommy", "black");
+        System.out.println("Here's the details of Animal = "+ obj.toString());
+
+    }
+}
+
+
